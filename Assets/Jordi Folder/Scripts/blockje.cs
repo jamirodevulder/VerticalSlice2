3ecce888b2060ab7723ce675658b01bad6b58adf
jvdlem <jordivanderlem@gmail.com>
2020-01-16 11:00:35 +0100

    Player controller added

---
+++ Assets/Jordi Folder/Scripts/blockje.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockje : MonoBehaviour
+{
+    public Rigidbody rb;
+    public long force = -10000000000000000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector3(0, force, 0)); 
+    }
+}
