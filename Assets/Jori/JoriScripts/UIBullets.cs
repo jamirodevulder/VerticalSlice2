351c19cbf54acdb3353c65d0cc07cd683611f292
Jorikos <26204@ma-web.nl>
2020-01-16 12:42:31 +0100

    Bullets

--- Assets/Jori/JoriScripts/UIBullets.cs
+++ Assets/Jori/JoriScripts/UIBullets.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 public class UIBullets : MonoBehaviour
 {
     private int currentBullet;
-    private Sprite activeBullet;
-    private Sprite inactiveBullet;
+    private Text bulletText;
+    [SerializeField] private Sprite activeBullet;
+    [SerializeField] private Sprite inactiveBullet;
     [SerializeField] private Image[] bullets = new Image[7];
     [SerializeField] private GameObject[] bulletGameObjects = new GameObject[7];
 
@@ -18,6 +19,9 @@ public class UIBullets : MonoBehaviour
             bulletGameObjects[i] = GameObject.Find("BulletGameObject " + i); //Pakt parent gameobjects van bullets
             bullets[i] = GameObject.Find("BulletGameObject " + i).GetComponentInChildren<Image>(); //Pakt bullet images
         }
+        currentBullet = 0; // Begint bij 0, om gebruikt 
+        bulletText = GameObject.Find("BulletTextGameObject").GetComponentInChildren<Text>();
+
     }
 
     private void Update()
